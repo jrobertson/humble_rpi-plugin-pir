fc9ce53426df76769067f78f3a58e0bc0c04a473
Your Name <you@example.com>
2015-06-13 21:23:56 +0100

    1st commit

---
+++ humble_rpi-plugin-pir.gemspec
@@ -0,0 +1,15 @@
+Gem::Specification.new do |s|
+  s.name = 'humble_rpi-plugin-pir'
+  s.version = '0.1.0'
+  s.summary = 'A humble_rpi plugin which detects motion for 1 or more PIR sensor.'
+  s.description = 'This plugin can be tested in isolation with the Raspberry Pi.'
+  s.authors = ['James Robertson']
+  s.files = Dir['lib/humble_rpi-plugin-pir.rb']
+  s.add_runtime_dependency('pi_piper', '~> 1.3', '>=1.3.2')  
+  s.add_runtime_dependency('chronic_duration', '~> 0.10', '>=0.10.6')
+  s.signing_key = '../privatekeys/humble_rpi-plugin-pir.pem'
+  s.cert_chain  = ['gem-public_cert.pem']
+  s.license = 'MIT'
+  s.email = 'james@r0bertson.co.uk'
+  s.homepage = 'https://github.com/jrobertson/humble_rpi-plugin-pir'
+end
---
+++ lib/humble_rpi-plugin-pir.rb
@@ -0,0 +1,66 @@
+#!/usr/bin/env ruby
+
+# file: humble_rpi-plugin-pir.rb
+
+require 'chronic_duration'
+require 'pi_piper'
+
+
+class HumbleRPiPluginPir
+  include PiPiper
+
+  def initialize(settings: {}, variables: {})
+
+    @pins = settings[:pins]
+    @duration = settings[:duration] || '1 minute'
+    @notifier = variables[:notifier]
+    @device_id = variables[:device_id] || 'pi'
+      
+    at_exit do
+      
+      @pins.each do |pin|
+
+        uexp = open("/sys/class/gpio/unexport", "w")
+        uexp.write(pin)
+        uexp.close
+      
+      end
+    end
+
+    
+  end
+
+  def start()
+
+    count = 0
+    t1 = Time.now - ChronicDuration.parse('1 minute and 10 seconds')
+    
+    duration = @duration
+    notifier = @notifier
+    device_id = @device_id
+
+    @pins.each do |pin|
+      
+      after pin: pin.to_i, goes: :high do
+        
+        count += 1
+        
+        if Time.now > t1 + ChronicDuration.parse(duration)  then
+          
+          notifier.notice "%s/motion: detected %s times within the past %s" \
+                                                % [device_id, count, duration]
+          t1 = Time.now
+          count = 0
+        end
+
+      end
+    end
+    
+    PiPiper.wait
+    
+  end
+  
+  alias on_start start
+  
+  
+end
